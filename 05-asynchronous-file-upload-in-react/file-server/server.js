cb7eaed5d704c2cf44b118ca8143ce0ac09fafea
Martin Xu <martin.xu.contractor@move.com>
2021-07-04 21:07:46 -0700

    successfully decoded the image file

--- 05-asynchronous-file-upload-in-react/file-server/server.js
+++ 05-asynchronous-file-upload-in-react/file-server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
+const fileType = require("file-type");
 
 const upload = multer({
   dest: "uploads/", // this saves your file into a directory called "uploads"
@@ -15,9 +16,20 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/hello", (req, res) => {
   res.send("HELLO");
 });
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post("/upload", upload.single("file"), async (req, res) => {
   console.log(req.headers);
   console.log(req.body);
+
+  const imageData = req.body.file.substr(23, req.body.file.length);
+
+  console.log(`imageData = ${imageData}`);
+
+  const buffer = Buffer.from(imageData, "base64");
+  const fileInfo = await fileType.fromBuffer(buffer);
+  console.log(`fileInfo = ${JSON.stringify(fileInfo)}`);
+  const detectedExt = fileInfo.ext;
+  const detectedMime = fileInfo.mime;
+
   res.send("OKAY");
 });
 
