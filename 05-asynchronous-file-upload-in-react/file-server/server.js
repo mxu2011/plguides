ef3524d7d594a01733852735307751705dfa7dbb
Martin Xu <martin.xu.contractor@move.com>
2021-07-04 20:45:17 -0700

    added .gitignore for file-server and added loggings to server.js

--- 05-asynchronous-file-upload-in-react/file-server/server.js
+++ 05-asynchronous-file-upload-in-react/file-server/server.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors')
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
 
 const upload = multer({
-  dest: 'uploads/' // this saves your file into a directory called "uploads"
-}); 
+  dest: "uploads/", // this saves your file into a directory called "uploads"
+});
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // It's very crucial that the file name matches the name attribute in your html
-app.get('/hello', (req, res) => {
-    res.send("HELLO");
-})
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.send("OKAY")
+app.get("/hello", (req, res) => {
+  res.send("HELLO");
+});
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log(req.headers);
+  console.log(req.body);
+  res.send("OKAY");
 });
 
 app.listen(5000);
